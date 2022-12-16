56582bc0eed1b52bd39d950224037e2e684ea0dd
guicompeng <guicompeng@gmail.com>
2022-12-15 21:51:34 -0300

    add nome.txt e acentos

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
  - Autor: Guilherme de Assis
 
 ## Arquitetura
-  - A classe `Funcionario` possui atributos gerais de todos funcionarios, além de métodos abstratos `getSalario` e `getBeneficio`, que são comuns a todo tipo de funcionário.
+  - A classe `Funcionario` possui atributos gerais de todos funcionários, além de métodos abstratos `getSalario` e `getBeneficio`, que são comuns a todo tipo de funcionário.
   - A classe `FuncionarioBeneficio` herda de  `Funcionario` e possui um novo método abstrato: `getBeneficioBase`
   - As classes `Vendedor` e `Secretario` herdam de `FuncionarioBeneficio`.
   - A classe `Gerente` herda diretamente de `Funcionario`, pois gerentes não precisam implementar o método `getBeneficioBase`.
@@ -14,4 +14,5 @@
   - A classe `Vendedor` possui um ArrayList de `vendas` da classe `Venda`
   - A classe `Empresa` possui um nome e um ArrayList de `funcionarios` da classe `Funcionario`.
   - A classe `Relatorio` possui 6 métodos estáticos de relatórios gerais (ex: retorna o funcionário que mais recebeu no mês)
-  - A `classe` Teste realiza as instâncias e testa o projeto.
+  - A classe `Teste` realiza as instâncias e testa o projeto.
+  
--- src/main/java/com/guilherme/empresa/Empresa.java
+++ src/main/java/com/guilherme/empresa/Empresa.java
@@ -28,11 +28,11 @@ public class Empresa {
                 vendedores.add((Vendedor) f);
             }
         }
-        System.out.println("Total pago (salario e beneficio) em 12/2021: " + Relatorio.totalPagoComBeneficios(funcionarios, 12, 2021));
-        System.out.println("Total pago (somente salario) em 12/2021: " + Relatorio.totalPagoSemBeneficios(funcionarios, 12, 2021));
-        System.out.println("Total pago (somente beneficios) em 12/2021: " + Relatorio.totalBeneficios(funcionariosComBeneficios, 12, 2021));
-        System.out.println("Funcionario que mais recebeu em 12/2021: " + Relatorio.funcionarioQueMaisRecebeu(funcionarios, 12, 2021).getNome());
-        System.out.println("Funcionario que mais recebeu beneficio em 12/2021: " + Relatorio.funcionarioQueMaisRecebeuBeneficio(funcionariosComBeneficios, 12, 2021));
+        System.out.println("Total pago (salários e benefícios) em 12/2021: " + Relatorio.totalPagoComBeneficios(funcionarios, 12, 2021));
+        System.out.println("Total pago (somente salários) em 12/2021: " + Relatorio.totalPagoSemBeneficios(funcionarios, 12, 2021));
+        System.out.println("Total pago (somente benefícios) em 12/2021: " + Relatorio.totalBeneficios(funcionariosComBeneficios, 12, 2021));
+        System.out.println("Funcionário que mais recebeu em 12/2021: " + Relatorio.funcionarioQueMaisRecebeu(funcionarios, 12, 2021).getNome());
+        System.out.println("Funcionário que mais recebeu benefício em 12/2021: " + Relatorio.funcionarioQueMaisRecebeuBeneficio(funcionariosComBeneficios, 12, 2021));
         System.out.println("Vendedor que mais vendeu em 12/2021: " + Relatorio.vendedorQueMaisVendeu(vendedores, 12, 2021).getNome());
         System.out.println("Vendedor que mais vendeu em 01/2022: " + Relatorio.vendedorQueMaisVendeu(vendedores, 1, 2022).getNome());
 
